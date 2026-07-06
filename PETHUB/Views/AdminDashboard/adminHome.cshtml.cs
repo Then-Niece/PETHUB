9683768c8aaefd72a17b0579b9d043370e279a16
rob-arch <robert081104campos@gmail.com>
2026-07-06 18:23:29 +0800

    # created userDashboard folder # moved Members and Users folders containing crud to userDashboard folder # created adminHome page inside the userDashboard folder

---
+++ PETHUB/Views/AdminDashboard/adminHome.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PETHUB.Views.AdminDashboard
+{
+    public class adminHomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
