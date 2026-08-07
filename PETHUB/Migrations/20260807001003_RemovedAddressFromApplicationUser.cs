e2bfd4c9dc89d2caa23f249d12e68f6fae55efde
rob-arch <robert081104campos@gmail.com>
2026-08-06 17:19:21 -0700

    - Removed Address Field from ApplicationUser model - Added Migration RemovedAddressFromApplicationUser

---
+++ PETHUB/Migrations/20260807001003_RemovedAddressFromApplicationUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PETHUB.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedAddressFromApplicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
